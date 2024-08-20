c340170ef6364995ba712d4b50359763a3410539
hoseasamuel <debbiemonna5@gmail.com>
2024-08-20 12:45:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -199,59 +199,7 @@ else
 fi
 }
 
-uninstall_theme() {
-  echo -e "                                                       "
-  echo -e "${BLUE}[+] =============================================== [+]${NC}"
-  echo -e "${BLUE}[+]                    DELETE THEME                 [+]${NC}"
-  echo -e "${BLUE}[+] =============================================== [+]${NC}"
-  echo -e "                                                       "
-if (( $EUID != 0 )); then
-    echo "Silahkan masuk ke direktori root"
-    exit
-fi
-    cd /var/www/pterodactyl
 
-    php artisan down
-
-    rm -r /var/www/pterodactyl/resources
-
-    curl -L https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz | tar -xzv
-
-    chmod -R 755 storage/* bootstrap/cache
-
-    composer install --no-dev --optimize-autoloader
-
-    php artisan view:clear
-
-    php artisan config:clear
-
-    php artisan migrate --seed --force
-
-    chown -R www-data:www-data /var/www/pterodactyl/*
-
-    php artisan queue:restart
-
-    php artisan up
-}
-while true; do
-    read -p "apakah kamu yakin untuk mengUninstall theme ? [y/n] " yn
-    case $yn in
-        [Yy]* ) repairPanel; break;;
-        [Nn]* ) exit;;
-        * ) echo "silahkan pilih (y/yes) (n/no).";;
-    esac
-done
-  echo -e "                                                       "
-  echo -e "${GREEN}[+] =============================================== [+]${NC}"
-  echo -e "${GREEN}[+]                 DELETE THEME SUKSES             [+]${NC}"
-  echo -e "${GREEN}[+] =============================================== [+]${NC}"
-  echo -e ""
-  sleep 5
-else
-  echo ""
-  echo "Pilihan tidak valid. silahkan pilih 1/2/3."
-fi
-}
 ################################################################################################
 # INSTALL PANEL / WINGS / UNISTALL PANEL / WINGS
 install_wings() {
